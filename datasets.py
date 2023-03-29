1b6764251301a1754773cc64b85cb01982f33648
mcabezas <mcabezas@eia.udg.edu>
2023-03-29 16:33:24 +1100

    tweaks to shapes dataset + location dataset

--- datasets.py
+++ datasets.py
@@ -12,7 +12,7 @@ class GeometricDataset(Dataset):
     ):
         self.len = n_samples * 2
         self.max_x, self.max_y, self.max_z = im_size
-        self.max_side = np.min(im_size) / 2
+        self.max_side = np.min(im_size)
         self.min_side = self.max_side / scale_ratio
         self.min_back = min_back
         self.max_back = max_back
@@ -127,6 +127,88 @@ class ShapesDataset(GeometricDataset):
                 # Spheres
                 target_data = 1
                 mask = self._sphere_mask(x, y, z, cx, cy, cz, s / 2)
+
+            data[mask] = foreground[mask]
+
+        return data, target_data
+
+
+class LocationDataset(GeometricDataset):
+    def __init__(
+            self, im_size, n_samples=1000, scale_ratio=2.5,
+            min_back=100, max_back=200, fore_ratio=2, seed=None
+    ):
+        # Init
+        super().__init__(
+            im_size, n_samples, scale_ratio, min_back, max_back, fore_ratio,
+            seed
+        )
+        self.shapes = []
+        self.masks = []
+        self.labels = []
+        if self.seed is not None:
+            np.random.seed(self.seed)
+            for i in range(self.len):
+                cx, cy, cz, r = self._coordinates()
+                background, foreground = self._gaussian_images()
+                x, y, z = self._image_grid()
+
+                if i < n_samples:
+                    # Spheres (top-left)
+                    self.labels.append(0)
+                    mask = self._sphere_mask(x, y, z, cx, cy, cz, r)
+                else:
+                    # Spheres (bottom-right)
+                    self.labels.append(1)
+                    mask = self._sphere_mask(
+                        x, y, z,
+                        cx + self.max_x / 2,
+                        cy + self.max_y / 2,
+                        cz + self.max_z / 2,
+                        r
+                    )
+
+                background[mask] = foreground[mask]
+                self.masks.append(mask)
+                self.shapes.append(background)
+
+    def _coordinates(self):
+        side_range = self.max_side - self.min_side
+        cx = self.max_x * 0.25 + np.random.rand(1) * self.max_x * 0.1
+        cy = self.max_y * 0.25 + np.random.rand(1) * self.max_y * 0.1
+        cz = self.max_z * 0.25 + np.random.rand(1) * self.max_z * 0.1
+        r = min(
+            cx - 1, cy - 1, cz - 1,
+            self.max_x * 0.5 - cx - 1,
+            self.max_y * 0.5 - cy - 1,
+            self.max_z * 0.5 - cz - 1,
+            np.random.rand(1) * side_range + self.min_side
+        )
+
+        return cx, cy, cz, r
+
+    def __getitem__(self, index):
+        if len(self.shapes) > 0:
+            data = self.shapes[index]
+            target_data = (self.labels[index], self.masks[index])
+        else:
+            cx, cy, cz, r = self._coordinates()
+            data, foreground = self._gaussian_images()
+            x, y, z = self._image_grid()
+            if index < (self.len / 2):
+                # Spheres (top-left)
+                target_data = 0
+                mask = self._sphere_mask(x, y, z, cx, cy, cz, r)
+            else:
+                # Spheres (bottom-right)
+                target_data = 1
+                mask = self._sphere_mask(
+                    x, y, z,
+                    cx + self.max_x / 2,
+                    cy + self.max_y / 2,
+                    cz + self.max_z / 2,
+                    r
+                )
 
             data[mask] = foreground[mask]
 
